a631eedb8ee37d00f99e7617c7852e5b283fec67
unknown <fefe.borrometi@gmail.com>
2023-07-03 17:48:22 +0200

    Esercitazione 03 luglio 2023

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container1">1</div>
+      <div className="container2">2</div>
+      <div className="container3">3</div>
+    </div>
+  );
+}
+
+export default App;
